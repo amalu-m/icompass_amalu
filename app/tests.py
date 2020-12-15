84c4554aba1448b6be33deef6f1aa6c82b00c49a
Amalu Mathew <amalulmathew@gmail.com>
2020-12-15 13:43:25 -0800

    modified the views file to different functions

--- app/tests.py
+++ app/tests.py
@@ -1,26 +1,36 @@
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
-from app.views import inputData
+from app.views import inputData, hasSqlInjection, buildResponse
 import json
 
+
 class InputTestCase(TestCase):
-    def test_sanitized_inputData(self):
+
+    def test_inputData(self):
         factory = APIRequestFactory()
         request = factory.post('/v1/sanitized/input', {'payload': 'hello'})
         response = inputData(request)
         dict = json.loads(response.content)
         self.assertEqual(dict['result'], 'sanitized')
 
-    def test_unsanitized_inputData_lower_case(self):
-        factory = APIRequestFactory()
-        request = factory.post('/v1/sanitized/input', {'payload': 'select'})
-        response = inputData(request)
+    def test_hasSqlInjectionTrue(self):
+        input = 'select'
+        response = hasSqlInjection(input)
+        self.assertEqual(True, True)
+
+    def test_hasSqlInjectionFalse(self):
+        input = 'hello'
+        response = hasSqlInjection(input)
+        self.assertEqual(False,False)
+
+    def test_buildResponseunsanitized(self):
+        input = True
+        response = buildResponse(input)
         dict = json.loads(response.content)
         self.assertEqual(dict['result'], 'unsanitized')
 
-    def test_unsanitized_inputData_upper_case(self):
-        factory = APIRequestFactory()
-        request = factory.post('/v1/sanitized/input', {'payload': 'DELETE'})
-        response = inputData(request)
+    def test_buildResponsesanitized(self):
+        input = False
+        response = buildResponse(input)
         dict = json.loads(response.content)
-        self.assertEqual(dict['result'], 'unsanitized')
+        self.assertEqual(dict['result'], 'sanitized')
--- app/views.py
+++ app/views.py
@@ -3,27 +3,38 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from rest_framework.exceptions import APIException
 import re
-#import pdb
+import pdb
 
 @csrf_exempt #disabling in development/Postman
 def inputData(request):
-    pattern = '(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE|UNION( +ALL){0,1})'
-
     #only consider POST requests (per requirement)
-    if request.method =='POST':
+    if request.method == 'POST':
         #get value for key = 'payload'
         query = request.POST.get('payload', None)
         if query is None:
             raise APIException("There was a problem with input data")
 
-        #check for sql commands using regex
-        match = re.search(pattern, query, flags=re.IGNORECASE)
+        hasMatchingPattern = hasSqlInjection(query)
+        result = buildResponse(hasMatchingPattern)
+        return result
+
+def hasSqlInjection(input):
+    pattern = '(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE|UNION( +ALL){0,1})'
+    #check for sql commands using regex
+    match = re.search(pattern, input, flags = re.IGNORECASE)
+    # pdb.set_trace()
+    if match is None:
+        return False
+    else:
+        return True
+
+def buildResponse(hasMatchingPattern):
         response = {}
         #check if there are any matches
-        if match is None:
-            response['result'] = 'sanitized'
-        else:
+        if hasMatchingPattern:
             response['result'] = 'unsanitized'
+        else:
+            response['result'] = 'sanitized'
 
         #return response
         return JsonResponse(response)
