a80a624b794b06ab5657f6d19dede3c5c7c2ea10
Amalu Mathew <amalulmathew@gmail.com>
2020-12-14 17:10:18 -0800

    Added test cases.

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from app.views import inputData
+import json
 
-# Create your tests here.
+class InputTestCase(TestCase):
+    def test_sanitized_inputData(self):
+        factory = APIRequestFactory()
+        request = factory.post('/v1/sanitized/input', {'payload': 'hello'})
+        response = inputData(request)
+        dict = json.loads(response.content)
+        self.assertEqual(dict['result'], 'sanitized')
+
+    def test_unsanitized_inputData_lower_case(self):
+        factory = APIRequestFactory()
+        request = factory.post('/v1/sanitized/input', {'payload': 'select'})
+        response = inputData(request)
+        dict = json.loads(response.content)
+        self.assertEqual(dict['result'], 'unsanitized')
+
+    def test_unsanitized_inputData_upper_case(self):
+        factory = APIRequestFactory()
+        request = factory.post('/v1/sanitized/input', {'payload': 'DELETE'})
+        response = inputData(request)
+        dict = json.loads(response.content)
+        self.assertEqual(dict['result'], 'unsanitized')
