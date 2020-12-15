f72d9a98f7d870f1dbd7d0dda412b719a12a5e59
Amalu Mathew <amalulmathew@gmail.com>
2020-12-15 14:05:55 -0800

    Added test to assert exception type

--- app/tests.py
+++ app/tests.py
@@ -1,17 +1,23 @@
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
 from app.views import inputData, hasSqlInjection, buildResponse
+from rest_framework.exceptions import APIException
 import json
-
 
 class InputTestCase(TestCase):
 
-    def test_inputData(self):
+    def test_inputData_valid_data(self):
         factory = APIRequestFactory()
         request = factory.post('/v1/sanitized/input', {'payload': 'hello'})
         response = inputData(request)
         dict = json.loads(response.content)
         self.assertEqual(dict['result'], 'sanitized')
+
+    def test_inputData_missing_payload_key(self):
+        factory = APIRequestFactory()
+        request = factory.post('/v1/sanitized/input', {'key': 'value'})
+        with self.assertRaises(APIException):
+            inputData(request)
 
     def test_hasSqlInjectionTrue(self):
         input = 'select'
@@ -21,7 +27,7 @@ class InputTestCase(TestCase):
     def test_hasSqlInjectionFalse(self):
         input = 'hello'
         response = hasSqlInjection(input)
-        self.assertEqual(False,False)
+        self.assertEqual(False, False)
 
     def test_buildResponseunsanitized(self):
         input = True
